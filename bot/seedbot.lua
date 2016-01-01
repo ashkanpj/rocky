280248b54172096d88d7f138f2b4f532b792e8a4
ashkanpj <ashkanpj.ap33@gmail.com>
2016-01-01 19:42:05 +0330

    rocky

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {81444630 tonumber(our_id)},--Sudo users
+    sudo_users = {81444630},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
